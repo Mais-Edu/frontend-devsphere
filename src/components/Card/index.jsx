78209dc886964ca1a9a649c80408f5cd4edda6f9
chidekina <cesar.nagano1@gmail.com>
2025-09-09 19:01:29 -0300

    fix: add card list json

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,11 +1,11 @@
-const Card = (icon, title, description, link, textColor, linkDescription) => {
+const Card = ({ key,icon, title, description, link, textColor, linkDescription }) => {
     return ( 
-        <div className="bg-slate-700 p-8 flex flex-col gap-6">
+        <li className="bg-slate-700 p-8 flex flex-col gap-6" key={key}>
             <img src={icon} />
             <h3 className="font-bold">{title}</h3>
             <p className="text-slate-300">{description}</p>
             <a href={link} className={`text-sm ${textColor}`}>{linkDescription} →</a>
-        </div>
+        </li>
      );
 }
  
---
+++ src/components/HighlightHome/index.jsx
@@ -0,0 +1,12 @@
+const HighlightHome = () => {
+    return ( 
+        <section className="h-[558px] flex flex-col justify-center items-center gap-16">
+            <h1 className="font-bold text-4xl">Destaques</h1>
+            <ul>
+                
+            </ul>
+        </section>
+     );
+}
+ 
+export default HighlightHome;

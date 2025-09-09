b1eb234cf2226faf92c232416013a5f9466e57e0
chidekina <cesar.nagano1@gmail.com>
2025-09-09 08:54:36 -0300

    feat: merge

--- src/components/CallToRegister/index.jsx
+++ src/components/CallToRegister/index.jsx
@@ -5,7 +5,7 @@ const CallToRegister = () => {
         <section className="h-72 flex flex-col justify-center items-center">
             <h1 className="font-bold text-3xl mb-4">Pronto para evoluir?</h1>
             <p className="text-lg text-slate-300 mb-8">Junte-se à maior comunidade de desenvolvedores do Brasil</p>
-            <Button className="bg-gradient-progress h-16 w-60 flex justify-center items-center font-semibold text-lg">
+            <Button className="bg-gradient-progress h-16 w-60 flex justify-center items-center font-semibold text-lg" href="#">
                 Criar conta gratuita
             </Button>
         </section>

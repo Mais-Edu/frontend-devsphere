6b42e043c5824322100024bd2ba99a8a8566212e
chidekina <cesar.nagano1@gmail.com>
2025-09-15 16:12:21 -0300

    resolve: merge conflict in TestPage - combine all components

--- src/pages/TestPage/index.jsx
+++ src/pages/TestPage/index.jsx
@@ -1,20 +1,14 @@
 import CallToRegister from "../../components/CallToRegister";
-<<<<<<< HEAD
 import InNumbers from "../../components/InNumbers";
 import WelcomeHome from "../../components/WelcomeHome";
-=======
 import HighlightHome from "../../components/HighlightHome";
->>>>>>> feature/HighlightHome
 
 const TestPage = () => {
     return ( 
         <>
-<<<<<<< HEAD
         <WelcomeHome />
         <InNumbers />
-=======
         <HighlightHome />
->>>>>>> feature/HighlightHome
         <CallToRegister />
         </>
      );
